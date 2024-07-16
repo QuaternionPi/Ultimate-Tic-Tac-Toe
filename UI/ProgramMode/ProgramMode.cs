cf0601d96decce8aaa8c8df756e3650397e08304
David Wiebe <drwho.david.wiebe@gmail.com>
2024-07-16 11:46:14 -0700

    Change ProgramMode to global namespace

--- UI/ProgramMode/ProgramMode.cs
+++ UI/ProgramMode/ProgramMode.cs
@@ -1,19 +1,7 @@
-using System.Numerics;
-using Raylib_cs;
-using UltimateTicTacToe.Game;
-
-namespace UltimateTicTacToe
+namespace UltimateTicTacToe.UI.ProgramMode;
+public interface IProgramMode : IDrawable, IUpdateable, ITransitionable
 {
-    namespace UI
-    {
-        namespace ProgramMode
-        {
-            public interface IProgramMode : IDrawable, IUpdateable, ITransitionable
-            {
-                public event SwitchToDel? SwitchTo;
-                public delegate void SwitchToDel(IProgramMode sender, IProgramMode switchTo);
-                public IProgramMode? Previous { get; }
-            }
-        }
-    }
+    public event SwitchToDel? SwitchTo;
+    public delegate void SwitchToDel(IProgramMode sender, IProgramMode switchTo);
+    public IProgramMode? Previous { get; }
 }
