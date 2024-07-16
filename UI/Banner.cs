4d7246bc224a0a6bc170a630aff6673d4e9e9ae3
David Wiebe <drwho.david.wiebe@gmail.com>
2024-07-15 20:23:54 -0700

    Change Banner to global namespace

--- UI/Banner.cs
+++ UI/Banner.cs
@@ -1,48 +1,43 @@
 using System.Numerics;
 using Raylib_cs;
 
-namespace UltimateTicTacToe
+namespace UltimateTicTacToe.UI;
+/*
+Displays a team's symbol and score
+*/
+public class Banner : IDrawable
 {
-    namespace UI
+    public Banner(Player player, Transform2D transform, bool active)
     {
-        /*
-        Displays a team's symbol and score
-        */
-        public class Banner : IDrawable
+        Player = player;
+        Transform = transform;
+        Active = active;
+        Font = Graphics.Text.GetFontDefault();
+        Tile = new Game.Tile(Player, Transform, false, 0);
+    }
+    public Transform2D Transform { get; }
+    public Player Player { get; }
+    public bool Active { get; }
+    protected Game.Tile Tile { get; }
+    protected Font Font { get; }
+    public void Draw()
+    {
+        if (Active == false)
         {
-            public Banner(Player player, Transform2D transform, bool active)
-            {
-                Player = player;
-                Transform = transform;
-                Active = active;
-                Font = Graphics.Text.GetFontDefault();
-                Tile = new Game.Tile(Player, Transform, false, 0);
-            }
-            public Transform2D Transform { get; }
-            public Player Player { get; }
-            public bool Active { get; }
-            protected Game.Tile Tile { get; }
-            protected Font Font { get; }
-            public void Draw()
-            {
-                if (Active == false)
-                {
-                    Graphics.Draw.OverrideDrawColor = true;
-                    Graphics.Draw.OverrideColor = Color.LIGHTGRAY;
-                }
-                Tile.Draw();
-                Graphics.Draw.OverrideDrawColor = false;
-                DrawScore();
-            }
-            protected void DrawScore()
-            {
-                string message = Player.Score.ToString();
-                float spacing = 3;
-                float fontSize = 80;
-                float messageWidth = Graphics.Text.MeasureTextEx(Font, message, fontSize, spacing).X;
-                Vector2 drawPosition = Transform.Position + new Vector2(-messageWidth / 2, 70);
-                Graphics.Text.DrawTextEx(Font, message, drawPosition, fontSize, spacing, Color.LIGHTGRAY);
-            }
+            Graphics.Draw.OverrideDrawColor = true;
+            Graphics.Draw.OverrideColor = Color.LIGHTGRAY;
         }
+        Tile.Draw();
+        Graphics.Draw.OverrideDrawColor = false;
+        DrawScore();
+    }
+    protected void DrawScore()
+    {
+        string message = Player.Score.ToString();
+        float spacing = 3;
+        float fontSize = 80;
+        float messageWidth = Graphics.Text.MeasureTextEx(Font, message, fontSize, spacing).X;
+        Vector2 drawPosition = Transform.Position + new Vector2(-messageWidth / 2, 70);
+        Graphics.Text.DrawTextEx(Font, message, drawPosition, fontSize, spacing, Color.LIGHTGRAY);
     }
 }
