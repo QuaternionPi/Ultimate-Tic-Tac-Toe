79349a83cf41080307554e037429a63e671e0b5a
David Wiebe <drwho.david.wiebe@gmail.com>
2024-07-15 20:22:02 -0700

    Change Transform to global namespace

--- Transform.cs
+++ Transform.cs
@@ -1,39 +1,37 @@
 using System.Numerics;
 using System.Text.Json.Serialization;
-using Raylib_cs;
 
-namespace UltimateTicTacToe
+namespace UltimateTicTacToe;
+
+/*
+Position, Rotation and Scale in 2D space
+*/
+public readonly struct Transform2D
 {
-    /*
-    Position, Rotation and Scale in 2D space
-    */
-    public readonly struct Transform2D
+    public Transform2D(Vector2 position, float rotation = 0, float scale = 1)
     {
-        public Transform2D(Vector2 position, float rotation = 0, float scale = 1)
-        {
-            Position = position;
-            Rotation = rotation;
-            Scale = scale;
-        }
-        [JsonInclude]
-        [JsonConverter(typeof(Json.Vector2Converter))]
-        public Vector2 Position
-        {
-            get;
-        }
-        [JsonInclude]
-        public float Rotation
-        {
-            get;
-        }
-        [JsonInclude]
-        public float Scale
-        {
-            get;
-        }
-        public Transform2D Translate(Vector2 delta)
-        {
-            return new Transform2D(Position + delta, Rotation, Scale);
-        }
+        Position = position;
+        Rotation = rotation;
+        Scale = scale;
+    }
+    [JsonInclude]
+    [JsonConverter(typeof(Json.Vector2Converter))]
+    public Vector2 Position
+    {
+        get;
+    }
+    [JsonInclude]
+    public float Rotation
+    {
+        get;
+    }
+    [JsonInclude]
+    public float Scale
+    {
+        get;
+    }
+    public Transform2D Translate(Vector2 delta)
+    {
+        return new Transform2D(Position + delta, Rotation, Scale);
     }
 }
