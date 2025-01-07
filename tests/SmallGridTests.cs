f177e243e86e28b48d417968e3a22e8dc1e93ee7
David Wiebe <drwho.david.wiebe@gmail.com>
2025-01-06 18:38:33 -0800

    Configure basic tests

---
+++ tests/SmallGridTests.cs
@@ -0,0 +1,20 @@
+﻿using UltimateTicTacToe;
+using UltimateTicTacToe.Game;
+
+namespace UltimateTicTacToeTests;
+
+public class SmallGridTests
+{
+    private readonly Grid<Tile> _grid;
+    public SmallGridTests()
+    {
+        _grid = new();
+    }
+    [Fact]
+    public void EmptyGridHasNineEmptyCells()
+    {
+        foreach(var row in _grid.Cells)
+            foreach (var cell in row)
+                Assert.Null(cell.Player);
+    }
+}
