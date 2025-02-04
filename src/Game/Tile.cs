a8223bd73a34024127244d8ff2f5bb011c0064c1
David Wiebe <drwho.david.wiebe@gmail.com>
2025-02-03 22:47:52 -0800

    Change Tile.Placeable to be a lambda, for performance

--- src/Game/Tile.cs
+++ src/Game/Tile.cs
@@ -3,13 +3,11 @@ using System.Text.Json.Serialization;
 namespace UltimateTicTacToe.Game;
 public class Tile : ICell<Tile>
 {
-    [JsonInclude]
     public Player.Token? Owner { get; }
-    public bool Placeable { get; }
+    public bool Placeable { get => Owner == null; }
     public Tile(Player.Token? player)
     {
         Owner = player;
-        Placeable = player == null;
     }
     public Tile Place(Player.Token? player)
     {
